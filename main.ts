b9a85d8cf5b767bf0005e686dc3b88ef7e899b66
Tsukasa (Real) <131947967+TsukasaFromPjskReal@users.noreply.github.com>
2023-05-09 10:11:58 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,10 @@
+let velocidad = 60
+maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, velocidad)
+basic.pause(3000)
+maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, velocidad)
+basic.pause(3000)
+maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, velocidad)
+basic.pause(3000)
+basic.forever(function () {
+	
+})
